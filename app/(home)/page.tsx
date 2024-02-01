2df0122d6df40608f23e4387b1ebdaebb8e8f05f
victoriaflb <victoriwflb@hotmail.com>
2024-02-01 20:48:41 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,8 +1,16 @@
-import Image from "next/image";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 import Header from "../_components/header";
 
 export default function Home() {
   return <div>
     <Header/>
+   <div className="p-5 pt-5">
+    <h2 className="text-xl font-bold">Olá, Miguel!</h2>
+    <p className="capitalize text-sm">{format(new Date(), "EEEE',' dd 'de' MMMM",{
+      locale: ptBR,
+    })}</p>
+
+   </div>
   </div>
 }
