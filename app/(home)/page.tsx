76809a9447ed4a98360e29a9075c252fcd8432b1
victoriaflb <victoriwflb@hotmail.com>
2024-02-01 21:02:41 -0300

    feat: add initial search component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,15 +2,24 @@ import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
 import Header from "../_components/header";
 
-export default function Home() {
-  return <div>
-    <Header/>
-   <div className="p-5 pt-5">
-    <h2 className="text-xl font-bold">Olá, Miguel!</h2>
-    <p className="capitalize text-sm">{format(new Date(), "EEEE',' dd 'de' MMMM",{
-      locale: ptBR,
-    })}</p>
+import Search from "@/app/(home)/_components/search";
 
-   </div>
-  </div>
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <div className="p-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Miguel!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
+
+      <div className="px-5 mt-6">
+        <Search />
+      </div>
+    </div>
+  );
 }
