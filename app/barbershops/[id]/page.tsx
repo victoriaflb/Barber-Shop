b7a738e44545997b7f80601bd4fb29cb85f43377
victoriaflb <victoriwflb@hotmail.com>
2024-02-05 19:27:39 -0300

    feat: add Barbershopinfo component

---
+++ app/barbershops/[id]/page.tsx
@@ -0,0 +1,41 @@
+
+import { db } from "@/app/_lib/prisma";
+import BarbershopInfo from "./_components/barbershop-info";
+
+interface BarbershopDetailsPageProps {
+  params: {
+    id?: string;
+  };
+}
+
+const BarbershopDetailsPage = async ({
+  params,
+}: BarbershopDetailsPageProps) => {
+
+
+  if (!params.id) {
+    // TODO: redirecionar para home page
+    return null;
+  }
+
+  const barbershop = await db.barbershop.findUnique({
+    where: {
+      id: params.id,
+    },
+    include: {
+      services: true,
+    },
+  });
+
+  if (!barbershop) {
+    // TODO: redirecionar para home page
+    return null;
+  }
+
+  return (
+    <BarbershopInfo barbershop={barbershop} />
+  );
+
+};
+
+export default BarbershopDetailsPage;
